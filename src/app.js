8fbf6c9c6076fc3d4df1573eb0c0fde40f0a919f
anselmosz <anselmoma2005@outlook.com>
2026-09-22 13:09:10 -0300

    test: ajustado chamada de verificação da assinatura HMAC antes das rotas - removida chamada do parse na linha 11

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import deployRouter from "./modules/deploy/deploy.route.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
 
 app.use(express.json({
   verify: (req, res, buf) => {
